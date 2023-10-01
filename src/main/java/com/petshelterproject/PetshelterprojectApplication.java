6efd8fed9845a369619c1c896fb46583cf5dddd3
t3hVega <ivan.laganchuk@gmail.com>
2023-10-01 14:58:21 +0300

    Основа

---
+++ src/main/java/com/petshelterproject/PetshelterprojectApplication.java
@@ -0,0 +1,13 @@
+package com.petshelterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshelterprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshelterprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petshelterproject/PetshelterprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshelterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshelterprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
