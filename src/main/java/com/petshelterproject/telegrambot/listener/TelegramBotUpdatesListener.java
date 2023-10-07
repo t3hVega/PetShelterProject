a92bef5a2794f7345b28efd4d669a36dc99498c3
t3hVega <ivan.laganchuk@gmail.com>
2023-10-07 20:00:27 +0300

    Основа для отправки обратных сообщений

--- src/main/java/com/petshelterproject/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/petshelterproject/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,10 +2,11 @@ package com.petshelterproject.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.petshelterproject.telegrambot.sender.TelegramMessageSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -16,8 +17,16 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Autowired
     private TelegramBot telegramBot;
+    private TelegramMessageSender telegramMessageSender;
+
+    public TelegramBotUpdatesListener (
+            TelegramBot telegramBot,
+            TelegramMessageSender telegramMessageSender
+    ) {
+        this.telegramBot = telegramBot;
+        this.telegramMessageSender = telegramMessageSender;
+    }
 
     @PostConstruct
     public void init() {
@@ -28,7 +37,20 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            Message message = update.message();
+            String text = update.message().text();
+            long chatId = update.message().chat().id();
+            switch (text) {
+                case "/placeholder1":
+                    telegramMessageSender.send(chatId, "Заглушка 1");
+                    break;
+                case "/placeholder2":
+                    telegramMessageSender.send(chatId, "Заглушка 2");
+                    break;
+                default:
+
+            }
+
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
